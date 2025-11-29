148abd0051ddacfcde4b8a45a0566691e2b22373
Aslam Najeebdeen <aslamnajeem@gmail.com>
2025-11-29 23:07:46 +0530

    Updated wezterm configs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,17 +7,26 @@ config.line_height = 1.2
 config.font = wezterm.font("JetBrains Mono")
 config.color_scheme = "Catppuccin Macchiato"
 
+-- Colors
 config.colors = {
 	cursor_bg = "#7aa2f7",
 	cursor_border = "#7aa2f7",
 }
 
+-- Appearance
 --config.window_background_opacity = 0.99
 --config.macos_window_background_blur = 15
 
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 30,
+	right = 10,
+}
+
+-- Keybindings
 config.keys = {
 	{
 		key = "w",
@@ -40,5 +49,9 @@ config.keys = {
 		action = wezterm.action.SendString("clear\n"),
 	},
 }
+
+-- Misc
+config.max_fps = 120
+config.prefer_egl = true
 
 return config
