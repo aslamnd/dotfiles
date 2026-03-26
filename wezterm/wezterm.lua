be10c37b84438903595e1a2e401c41e677bb8210
Aslam Najeebdeen <aslamnajeem@gmail.com>
2026-03-26 17:22:37 +0530

    Enable bottom tab bar in WezTerm with minimal style

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- General
-config.font_size = 19
+config.font_size = 16
 config.line_height = 1.2
 config.font = wezterm.font("JetBrains Mono")
 config.color_scheme = "Catppuccin Macchiato"
@@ -18,9 +18,11 @@ config.colors = {
 --config.macos_window_background_blur = 15
 
 config.window_decorations = "RESIZE"
-config.enable_tab_bar = false
-
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
 config.window_padding = {
 	left = 30,
 	right = 10,
